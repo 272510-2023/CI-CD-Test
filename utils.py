e26a7878897a8b1a3de935a4d49aef65f141a7a2
Daria <272510@student.pwr.edu.pl>
2025-05-20 20:21:31 +0200

    Modyfikacja test_utils.py oraz dodanie poctawowych fukcji utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+"""Moduł zawierający podstawowe operacje matematyczne:
+dodawanie, odejmowanie, mnożenie i dzielenie.
+"""
+
+
+def add(a, b):
+    """Zwraca sumę a i b."""
+    return a + b
+
+
+def subtract(a, b):
+    """Zwraca różnicę a - b."""
+    return a - b
+
+
+def multiply(a, b):
+    """Zwraca iloczyn a i b."""
+    return a * b
+
+
+def divide(a, b):
+    """Zwraca iloraz a / b. Zakłada, że b != 0."""
+    return a / b
